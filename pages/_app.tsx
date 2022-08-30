f54473d87e3889ab4a0b896ff8223ee6c7b590ea
はりけん++ <92016933+hrkn63-hnm@users.noreply.github.com>
2022-08-30 12:10:07 +0900

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css"
+import type { AppProps } from "next/app"
 import Header from "../components/Header";
 import "swiper/css/bundle";
 
