a9f724c61897e238fa77f4fe0649c368dd08fdc6
hrkn63-hnm <demo43160@gmail.com>
2022-10-13 21:36:47 +0900

    直し

--- libs/client.ts
+++ libs/client.ts
@@ -1,11 +1,7 @@
 import { createClient } from "microcms-js-sdk"
 
-export interface MicroCMSClient {
-  serviceDomain: string;
-  apiKey: string | undefined;
-}
 
 export const client = createClient({
   serviceDomain: "hariken-blog",
-  apiKey: process.env.API_KEY,
+  apiKey: process.env.API_KEY || '',
 });
