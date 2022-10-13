a4996b1f6d94a7abf0115ef7beaaa7d6d7f385d6
hrkn63-hnm <demo43160@gmail.com>
2022-10-13 21:21:56 +0900

    apikeyのstring|undefined型の改善

--- libs/client.ts
+++ libs/client.ts
@@ -1,5 +1,10 @@
 import { createClient } from "microcms-js-sdk"
 
+export interface MicroCMSClient {
+  serviceDomain: string;
+  apiKey: string | undefined;
+}
+
 export const client = createClient({
   serviceDomain: "hariken-blog",
   apiKey: process.env.API_KEY,
