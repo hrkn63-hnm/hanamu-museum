fe1067b0c80618541b7e60c50ec8238d29b2d2aa
hrkn63-hnm <demo43160@gmail.com>
2022-10-13 21:12:22 +0900

    競合の解決

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,6 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import Header from "../components/Header";
->>>>>>> f80c79d (a)
-=======
->>>>>>> 64cc830 (first commit)
 import "swiper/css/bundle";
 
 function MyApp({ Component, pageProps }: AppProps) {
