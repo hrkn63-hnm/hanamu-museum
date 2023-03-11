33966d014f139ea0f86e9d1452f565a9a0ac24c6
hrkn63-hnm <demo43160@gmail.com>
2023-03-11 21:23:58 +0900

    works.idの設定/フォント設定

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,13 +6,22 @@ import "swiper/css/bundle";
 import Head from "next/head";
 import Link from "next/link";
 import { Analytics } from "@vercel/analytics/react";
+import { createTheme } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+      const theme = createTheme({
+        typography: {
+          fontFamily: ["Zen Maru Gothic"].join(","),
+        },
+      });
   return (
     <>
       <div className="bg-indigo-400 min-h-screen w-screen text-center bg-cover bg-center font-maru pb-6">
-        <Component {...pageProps} />
+        <ThemeProvider theme={theme}>
+          <Component {...pageProps} />
+        </ThemeProvider>
       </div>
     </>
   );
